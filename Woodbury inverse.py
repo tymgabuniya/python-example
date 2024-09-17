caae552e6ca0aae1c3fe10588d53bbb76a47977b
tymgabuniya <tym.gabuniya@gmail.com>
2024-09-17 06:14:42 +0100

    Update Woodbury inverse.py

--- Woodbury inverse.py
+++ Woodbury inverse.py
@@ -8,14 +8,6 @@ import numpy as np
 
 def WoodburyInv(B,Omega,D):
     
-    if (len(B.shape) != 2) | (len(Omega.shape) != 2) | (len(D.shape) != 2):
-        
-        print("ATTENTION: all the input arrays have to be two dimensional!")
-        
-    if (B.shape[1] != Omega.shape[0]) | (B.shape[0] != D.shape[0]):
-        
-        print("ATTENTION: check the dimensions of the input arrays for consistency!")
-    
     n = D.shape[0]
     aux1 = np.diag(np.divide(np.ones(n),np.diag(D)))
     aux2 = np.linalg.inv(Omega)
